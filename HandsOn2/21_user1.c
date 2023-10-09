64ca088ff3d98925d627d83c2459fa43c4676322
kalyaniverma <kalyani.verma900@gmail.com>
2023-10-10 01:39:38 +0530

    21st program (for user 1)

---
+++ HandsOn2/21_user1.c
@@ -0,0 +1,24 @@
+/*
+=======================================================================================================================================
+Name : 21_user1.c
+Author : Kalyani Verma
+Description : Write two programs so that both can communicate by FIFO -Use two way communications.
+Date: 10th Oct, 2023.
+=======================================================================================================================================
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+	int fd1, fd2;
+	char buff1[80], buff2[80];
+	fd1 = open("21_fifo1", O_WRONLY);
+	fd2 = open("21_fifo2", O_RDONLY);
+	printf("Enter the message: ");
+	scanf(" %[^\n]", buff1);
+	write(fd1, buff1, sizeof(buff1));
+	read(fd2, buff2, sizeof(buff2));
+	printf("The message from user 2: %s\n", buff2);
+}
