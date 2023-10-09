67c289ba69be73f4ffc0a4a3f098245d650f4338
kalyaniverma <kalyani.verma900@gmail.com>
2023-10-09 11:10:40 +0530

    15th program

--- HandsOn2/14.c
+++ HandsOn2/14.c
@@ -1,3 +1,13 @@
+/*
+=======================================================================================================================================
+Name : 14.c
+Author : Kalyani Verma
+Description : Write a simple program to create a pipe, write to the pipe, read from pipe and display on the monitor.
+Date: 08th Oct, 2023.
+=======================================================================================================================================
+*/
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -13,15 +23,15 @@ int main() {
 		while(1)
 		{
 			scanf("%c", &buf);
-			//count_buf = write(pipefd[1], &buf, 1);
-			//if(count_buf == 0) {
-			//	eof = 1;
-			//	break;
-		//	}
-			//else {
-			//	count_buf = read(pipefd[0], &buf, 1);
+			count_buf = write(pipefd[1], &buf, 1);
+			if(count_buf == 0) {
+				eof = 1;
+				break;
+			}
+			else {
+				count_buf = read(pipefd[0], &buf, 1);
 				if(buf == '\n') break;
-		//	}
+			}
 			printf("%c", buf);
 		}
 		printf("\n");
