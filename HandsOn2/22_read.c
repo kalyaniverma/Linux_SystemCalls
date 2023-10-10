95ff8321c6b256db789ba9aff41e218e0d4d91c4
kalyaniverma <kalyani.verma900@gmail.com>
2023-10-10 15:08:43 +0530

    22nd program for read end

---
+++ HandsOn2/22_read.c
@@ -0,0 +1,33 @@
+/*
+=======================================================================================================================================
+Name : 22.c
+Author : Kalyani Verma
+Description : Write a program to wait for data to be written into FIFO within 10 seconds, use select system call with FIFO.
+Date: 10th Oct, 2023.
+=======================================================================================================================================
+*/
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+int main() {
+	
+	int fd;
+	char buff[80];
+	fd_set rfds;
+	struct timeval tv;
+	tv.tv_sec = 10;
+
+	fd = open("22_fifo", O_RDONLY);
+	FD_ZERO(&rfds);
+	FD_SET(fd, &rfds);
+	if (!select(fd + 1, &rfds, NULL, NULL, &tv))
+		printf("No data is available for reading yet\n");
+	else {
+		printf("Data is available now\n");
+		read(fd, buff, sizeof(buff));
+		printf("Data from FIFO: %s\n", buff);
+	}
+}
