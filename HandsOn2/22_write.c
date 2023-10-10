aabe422b66e13cfcd318d495c8b2a5f7afb2952e
kalyaniverma <kalyani.verma900@gmail.com>
2023-10-10 15:09:32 +0530

    22nd program for write end

---
+++ HandsOn2/22_write.c
@@ -0,0 +1,21 @@
+/*
+=======================================================================================================================================
+Name : 20_write.c
+Author : Kalyani Verma
+Description : Write two programs so that both can communicate by FIFO -Use one way communication.
+Date: 09th Oct, 2023.
+=======================================================================================================================================
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+	int fd;
+	char buff[80];
+	fd = open("22_fifo",O_WRONLY);
+	printf("Enter the text:");
+	scanf(" %[^\n]", buff);
+	write(fd, buff, sizeof(buff));
+}
